7fff1b35e8c102a3aa7ffea1fdeb077ae52dc071
Automedon <42299107+Automedon@users.noreply.github.com>
2019-01-30 21:03:09 +0700

    Updates

---
+++ precache-manifest.2b250d03f9a8448de170087b191fd260.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1cd8bb2ee2d8debce1f",
+    "url": "/static/css/main.50727c1d.chunk.css"
+  },
+  {
+    "revision": "d1cd8bb2ee2d8debce1f",
+    "url": "/static/js/main.d1cd8bb2.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b801704ce0ed7bd6585144abb27b3b8b",
+    "url": "/index.html"
+  }
+];
